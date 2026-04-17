c79d851128f3dfa208a28608f8fa63b142ee71a8
fernanda <251216@ids.upchiapas.edu.mx>
2026-04-16 23:19:26 -0600

    para usar variables de entorno

--- src/config/db.js
+++ src/config/db.js
@@ -2,21 +2,23 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '1234',
-    database: process.env.DB_NAME || 'medialert_db',
+    host: process.env.DB_HOST, // Esto leerá el 52.71.230.113 de Railway
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    // Agregar esto ayuda a evitar errores de conexión en nubes
+    connectTimeout: 10000 
 });
 
 pool.getConnection((err, connection) => {
     if (err) {
-        console.error('❌ Error MySQL:', err.message);
+        console.error('❌ Error conexión MySQL:', err.message);
     } else {
-        console.log(`✅ Conectado a MySQL en: ${process.env.DB_HOST || 'localhost'}`);
+        console.log('✅ Conexión exitosa a la base de datos de AWS');
         connection.release();
     }
 });
